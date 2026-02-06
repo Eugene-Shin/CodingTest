c485138fc64c71048cf0d0b54c286227eb6f7a6c
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-02-06 19:02:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 1636 ms, Memory: 109300 KB -BaekjoonHub

---
+++ Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[][] arr = new String[N][2];
+        for(int i = 0; i < N; i++) {
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+        }
+        Arrays.sort(arr, (a, b) -> {
+            int aNum = Integer.parseInt(a[0]);
+            int bNum = Integer.parseInt(b[0]);
+            if(aNum == bNum)    return 0;
+            return aNum - bNum;
+        });
+        
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0]);
+            System.out.println(arr[i][1]);
+        }
+    }
+}

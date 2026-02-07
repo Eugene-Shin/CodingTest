bd93fbb00f526fa75e2a7b8e212d5d7389630205
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-02-08 00:12:56 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 14732 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int T = Integer.parseInt(br.readLine());
+            for(int i = 0; i < T; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                
+                int tA = a, tB = b;
+                int k = 0;
+                while(tB != 0) {
+                    k = tA % tB;
+                    tA = tB;
+                    tB = k;
+                }
+                
+                bw.write((a * b / tA) + "");
+                bw.newLine();
+            }
+        }
+    }
+}

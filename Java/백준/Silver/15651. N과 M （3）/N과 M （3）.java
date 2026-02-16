df0604212df568297a810650defed142055eabc3
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-02-16 16:51:38 +0900

    [Silver III] Title: N과 M (3), Time: 672 ms, Memory: 164488 KB -BaekjoonHub

---
+++ Java/백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            String[] nums = br.readLine().split("\\s");
+            int N = Integer.parseInt(nums[0]);
+            int M = Integer.parseInt(nums[1]);
+        
+            int[] permutation = new int[M];
+            dfs(0, N, permutation, bw);
+        }
+    }
+    
+    public static void dfs(int depth, int end, int[] permutation, BufferedWriter bw) throws IOException {
+        if(depth == permutation.length) {
+            for(int i = 0; i < permutation.length; i++) {
+                bw.write(permutation[i] + "");
+                if(i < permutation.length - 1)    bw.write(" ");
+            }
+            bw.newLine();
+            return;
+        }
+        for(int i = 0; i < end; i++) {
+            permutation[depth] = i + 1;
+            dfs(depth + 1, end, permutation, bw);
+        }
+    }
+}

108de42e05768a8dc5d33de7c793093ce8516f41
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-03-26 18:16:22 +0900

    [Silver III] Title: 두 수의 합, Time: 280 ms, Memory: 28784 KB -BaekjoonHub

--- Java/백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ Java/백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,22 +12,31 @@ public class Main {
             int n = Integer.parseInt(br.readLine());
             int[] num = new int[1000001];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < n; i++) {
+            for(int i = 1; i <= n; i++) {
                 num[Integer.parseInt(st.nextToken())]++;
             }
-                
+            
             int x = Integer.parseInt(br.readLine());
             
             int count = 0;
+            int left = 0;
+            int right = num.length - 1;
             
-            for(int i = 1; i <= x / 2; i++) {
-                if(i > 1000000 || x - i > 1000000) continue;
-                
-                if(i == x - i) {
-                    count += num[i] * (num[i] - 1) / 2;
-                } else {
-                    count += num[i] * num[x - i];
+            while(left < right) {
+                int sum = left + right;
+                if(sum == x) {
+                    count += num[left] * num[right];
+                    left++;
+                    right--;
+                } else if(sum < x) {
+                    left++;
+                } else if(sum > x) {
+                    right--;
                 }
+            }
+            if(num[left] >= 2) {
+                int temp = num[left];
+                count += temp * (temp - 1) / 2;
             }
             
             System.out.println(count + "");

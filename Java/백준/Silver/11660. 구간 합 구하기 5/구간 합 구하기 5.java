979ed60b011ff314e16476d2568896b23e236b02
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-03-30 14:36:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 776 ms, Memory: 124572 KB -BaekjoonHub

---
+++ Java/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            
+            int[][] matrix = new int[N + 1][N + 1];
+            for(int i = 1; i <= N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 1; j <= N; j++) {
+                    matrix[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            int[][] prefix = new int[N + 1][N + 1];
+            for(int i = 1; i <= N; i++) {
+                for(int j = 1; j <= N; j++) {
+                    prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + matrix[i][j];
+                }
+            }
+            
+            for(int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x1 = Integer.parseInt(st.nextToken());
+                int y1 = Integer.parseInt(st.nextToken());
+                int x2 = Integer.parseInt(st.nextToken());
+                int y2 = Integer.parseInt(st.nextToken());
+                
+                int sum = prefix[x2][y2] - prefix[x2][y1 - 1] - prefix[x1 - 1][y2] + prefix[x1 - 1][y1 - 1];
+                
+                
+                bw.write(sum + "");
+                bw.newLine();
+            }
+        }
+    }
+}

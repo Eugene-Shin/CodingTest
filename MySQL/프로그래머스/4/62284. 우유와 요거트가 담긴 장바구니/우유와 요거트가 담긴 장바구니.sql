008b91c2c11466663de40ecb40e264885b15ffdc
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-08-31 16:32:14 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT cart_id
+FROM cart_products
+WHERE cart_id IN (
+    SELECT DISTINCT cart_id
+    FROM cart_products
+    WHERE name = 'Milk'
+) AND cart_id IN (
+    SELECT DISTINCT cart_id
+    FROM cart_products
+    WHERE name = 'Yogurt'
+)
+ORDER BY cart_id ASC;
